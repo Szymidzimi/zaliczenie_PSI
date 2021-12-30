82c6d677da081f91aab9558cc80bc30e9a0b681e
Szymon N@pieraj <szymon222333@wp.pl>
2021-12-30 18:26:53 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,38 @@
-const theme=document.getElementById('swith');
+const theme=document.querySelector('#swith');
+const imgs=document.querySelectorAll('.photos');
+const prov=document.querySelector('.provv');
+const next=document.querySelector('.nextt');
 
-theme.addEventListener('click',function(){
+let krok=0;
+let koniec=imgs.length;
+
+let slider=()=>{
+    for(let i=0;i<koniec;i++){
+        imgs[i].classList.remove('actv');
+    }
+}
+//przyciski
+next.addEventListener('click',()=>{
+    krok++;
+    krok>=koniec? krok=0:null;
+    slider();
+    imgs[krok].classList.add('actv');
+})
+prov.addEventListener('click',()=>{
+    krok--;
+    krok<0? krok=koniec-1:null;
+    slider();
+    imgs[krok].classList.add('actv');
+})
+//automat
+setInterval(()=>{
+    krok++;
+    krok>=koniec? krok=0:null;
+    slider();
+    imgs[krok].classList.add('actv');
+},4000)
+// zmiana motywu na ciemnmy
+theme.addEventListener('click',()=>{
     document.body.classList.toggle('dark-theme');
     if(theme.classList.contains('fa-toggle-on')){
         theme.classList.remove('fa-toggle-on');
@@ -12,7 +44,7 @@ theme.addEventListener('click',function(){
     }
 })
 
-
+//menu zwijalne oraz reakcja na scroll
 $(document).ready(function(){
     $('.fa-bars').click(function(){
         $(this).toggleClass('fa-times');
@@ -22,5 +54,12 @@ $(document).ready(function(){
     $(window).on('load scroll',function(){
         $('.fa-bars').removeClass('fa-times');
         $('.nav').removeClass('nav-toggle');
+    
+        if($(window).scrollTop()>10){
+            $('header').addClass('header-act');
+        }
+        else{
+            $('header').removeClass('header-act');
+        }
     });
 });
